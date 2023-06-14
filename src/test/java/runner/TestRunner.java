04210b597b9e8aabdfc80c74ef1c0024fb9b1c7f
Dhvanika <dhvanika.patel@gmail.com>
2023-06-14 10:20:21 -0400

    Added Extent report and log properties file

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import utilities.ConfigReader;
 
-@CucumberOptions(plugin = { "pretty", "json:target/Report.json" }, // reporting purpose
+@CucumberOptions(plugin = { "pretty", "json:target/Report.json","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, // reporting purpose
 		monochrome = false, // console output
 		tags = "", // tags from feature file
 		features = { "src/test/resources/features" }, // location of feature files
