d25954ec9e091346a10cb19e6b3d5a9aeaecac40
Dhvanika <dhvanika.patel@gmail.com>
2024-01-29 16:03:23 -0500

    updated file

--- src/test/java/appHooks/Hooks.java
+++ src/test/java/appHooks/Hooks.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayInputStream;
 
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -17,6 +18,7 @@ import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
 import io.qameta.allure.Allure;
+import io.qameta.allure.model.Label;
 import utilities.ConfigReader;
 import utilities.Loggerload;
 
@@ -50,11 +52,21 @@ public class Hooks {
 		Capabilities capabilities = ((RemoteWebDriver) driver).getCapabilities();
         String browserName = capabilities.getBrowserName();
         String browserVersion = capabilities.getBrowserVersion();
+        Platform operatingSystem = capabilities.getPlatformName();
+        
 		io.qameta.allure.Allure.addAttachment("Browser Name", browserName);
         io.qameta.allure.Allure.addAttachment("Browser Version", browserVersion);
         
         scenario.attach(browserName, "text/plain", "Browser Name: ");
         scenario.attach(browserVersion, "text/plain","Browser Version: ");
+        
+        
+        Allure.getLifecycle().updateTestCase(testResult -> {
+            testResult.getLabels().add(new Label().setName("Browser Name").setValue(browserName));
+            testResult.getLabels().add(new Label().setName("Browser Version").setValue(browserVersion));
+            testResult.getLabels().add(new Label().setName("Operating System").setValue(operatingSystem.toString()));
+            
+        });
         
         String currentdir = System.getProperty("user.dir");
         Loggerload.info(currentdir);
